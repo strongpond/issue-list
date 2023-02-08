f5b0285cc844097c1a2d495dcbbfcb980d1d778f
euiyeonlee <dmldus1121@naver.com>
2023-02-08 20:44:19 +0900

    HOTFIX : setIssueListData error 해결

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,11 +1,11 @@
 import { useState, useEffect, useCallback } from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 
 import { getIssueList } from "../apis";
 import { issueListDataAtom } from "../atoms";
 
 const useFetch = (page) => {
-  const setIssueListData = useSetRecoilState(issueListDataAtom);
+  const [issueListData, setIssueListData] = useRecoilState(issueListDataAtom);
   const [hasMore, setHasMore] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -16,14 +16,19 @@ const useFetch = (page) => {
       if (!response) {
         throw new Error(`서버에 오류가 있습니다.`);
       }
-      setIssueListData((prev) => {
-        return prev.length > 0 ? [...prev, ...response] : response;
-      });
+
+      if (issueListData.length > 0) {
+        setIssueListData((prev) => [...prev, ...response]);
+      } else {
+        setIssueListData(response);
+      }
+
       setHasMore(response.length > 0);
       setIsLoading(false);
     } catch (e) {
       throw new Error(`오류입니다. ${e.message}`);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, setIssueListData]);
 
   useEffect(() => {
